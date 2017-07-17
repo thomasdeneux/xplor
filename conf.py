894c4b64053fa1aa6051520124015bee9babc66f
pc_eqYves\Elodie <elodie.ikkache@student.ecp.fr>
2017-07-17 14:44:28 +0200

    favicon finally added

--- conf.py
+++ conf.py
@@ -103,12 +103,13 @@ html_theme_options = {
     "linkcolor": "#852563"
 }
 html_title = "Xplor your data"
+html_favicon = "images/favicons/favicon1.ico"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 
 # -- Options for HTMLHelp output ------------------------------------------
 
