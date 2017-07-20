09b0103bea28605b8c6fe6db458ca07a1f426c68
pc_eqYves\Elodie <elodie.ikkache@student.ecp.fr>
2017-07-20 15:23:54 +0200

    Trying again!

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ a = xdata.Color('red')
 
 # -- Configuration for Read the docs --------------------------------------
 
-from unittest.mock import MagicMock
+from mock import Mock as MagicMock
 
 class Mock(MagicMock):
     @classmethod
--- xplor/tests.py
+++ xplor/tests.py
@@ -24,6 +24,7 @@ import numpy as np
 import pandas as pd
 import unittest
 
+
 import xdata
 
 
